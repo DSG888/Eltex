728bb2e3784d6716090ba54419cc035836cde67f
dsg <DSG>
2019-03-26 23:49:03 +0700

    13. add comment

--- 13.cross_compilation/build.sh
+++ 13.cross_compilation/build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+# Распаковка
 mkdir /tmp/rootfs
 mkdir /tmp/src
 tar -xf ./linux.tar.xz -C /tmp/src
 tar -xf ./busybox.tar.bz2 -C /tmp/src
 
+# Сборка busybox
 cd /tmp/src/busybox-1.30.1
 export "ARCH=arm"
 export "CROSS_COMPILE=arm-linux-gnueabihf-"
@@ -12,6 +14,7 @@ make defconfig
 make -j9
 make CONFIG_PREFIX=/tmp/rootfs install
 
+# Создание initramfs
 cd /tmp/rootfs
 echo -e \#"${1//$/\\$!}!""/bin/sh\n/bin/mount -t devtmpfs devtmpfs /dev\n/bin/mount -t proc none /proc\n/bin/mount -t sysfs none /sys\nexec 0</dev/console\nexec 1>/dev/console\nexec 2>/dev/console\n\nexec /bin/sh" > init
 chmod +x init
@@ -19,11 +22,16 @@ mkdir dev etc home mnt proc sys
 cd ./dev 
 sudo mknod sda b 8 0 
 sudo mknod console c 5 1
+#Можно вручную создать и упаковать
+#find . -print0 | cpio --null -ov --format=newc > initramfs_data.cpio
+#gzip ./initramfs_data.cpio
 
+# Сборка ядра
 cd /tmp/src/linux-5.0.4/
 export "ARCH=arm"
 export "CROSS_COMPILE=arm-linux-gnueabihf-"
 make menuconfig
 make CONFIG_BLK_DEV_INITRD=y CONFIG_INITRAMFS_COMPRESSION_GZIP=y CONFIG_INITRAMFS_SOURCE="/tmp/rootfs" -j9
 
+# Запуск
 qemu-system-arm -m 256 -M virt -kernel /tmp/src/linux-5.0.4/arch/arm/boot/zImage -initrd /tmp/src/linux-5.0.4/usr/initramfs_data.cpio -nographic -append "console=ttyAMA0"
